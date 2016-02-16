6607ce283d54442aeba03328016a5af84cbeaf01
root <root@vladimir.v.petukhov.vds.free-th.ru>
2016-02-16 19:52:28 +0300

    Added ticker.sh for crontab

---
+++ ticker/ticker.sh
@@ -0,0 +1,9 @@
+тт#!/bin/bash
+cd /var/www/ticker/ticker/
+php /var/www/ticker/ticker/Ticker.php
+/bin/sleep 15
+php /var/www/ticker/ticker/Ticker.php
+/bin/sleep 15
+php /var/www/ticker/ticker/Ticker.php
+/bin/sleep 15
+php /var/www/ticker/ticker/Ticker.php
